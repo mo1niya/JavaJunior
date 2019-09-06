436ede88f71e472ab07e5fe0e3ffe50497156bc4
mo1niya <mo1niya@yandex.ru>
2019-09-06 21:09:44 +0300

    First commit

---
+++ ru.perepelitsina/part1/lesson02/task1/WithoutArgs1.java
@@ -0,0 +1,8 @@
+package ru.perepelitsina.part1.lesson02.task1;
+
+public class WithoutArgs1 {
+    public static void main(String[] args) {
+        int liters = 50;
+        System.out.println(liters*43 + " рубрей");
+    }
+}
