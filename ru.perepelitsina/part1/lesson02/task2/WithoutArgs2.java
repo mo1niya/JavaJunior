09a874f103606a4f3977707e2a271b6ef95bd9a3
mo1niya <mo1niya@yandex.ru>
2019-09-06 21:14:37 +0300

    First commit

---
+++ ru.perepelitsina/part1/lesson02/task2/WithoutArgs2.java
@@ -0,0 +1,9 @@
+package ru.perepelitsina.part1.lesson02.task2;
+
+public class WithoutArgs2 {
+    public static void main(String[] args) {
+        int zp = 70000;
+        int itog = zp/100*13;
+        System.out.println(zp-itog + " рубрей");
+    }
+}
