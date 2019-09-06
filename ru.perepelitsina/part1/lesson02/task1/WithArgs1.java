0f31adee9e6e7942c783607a3465721040268daf
mo1niya <mo1niya@yandex.ru>
2019-09-06 18:11:52 +0300

    Revert "First commit"
    This reverts commit 7be10cfadd57332d4072ea6ae00ef63f42d5ad42.

--- ru.perepelitsina/part1/lesson02/task1/WithArgs1.java
+++
@@ -1,7 +0,0 @@
-package ru.perepelitsina.part1.lesson02.task1;
-
-public class WithArgs1 {
-    public static void main(String[] args) {
-        System.out.println(Integer.parseInt(args[0])*43 + " рубрей");
-    }
-}
