4862773dc26284ada9378688bba1cc62dc8cb098
mo1niya <mo1niya@yandex.ru>
2019-09-06 21:14:48 +0300

    First commit

---
+++ ru.perepelitsina/part1/lesson02/task3/WithArgs3.java
@@ -0,0 +1,7 @@
+package ru.perepelitsina.part1.lesson02.task3;
+
+public class WithArgs3 {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt(args[0])/60/60 + " час");
+    }
+}
