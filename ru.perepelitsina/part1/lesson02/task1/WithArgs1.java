7be10cfadd57332d4072ea6ae00ef63f42d5ad42
mo1niya <mo1niya@yandex.ru>
2019-09-06 17:59:41 +0300

    First commit

---
+++ ru.perepelitsina/part1/lesson02/task1/WithArgs1.java
@@ -0,0 +1,7 @@
+package ru.perepelitsina.part1.lesson02.task1;
+
+public class WithArgs1 {
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt(args[0])*43 + " рубрей");
+    }
+}
