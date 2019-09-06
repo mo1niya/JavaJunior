059ce4a447bed018a7dc8d8f45358f56905319c2
mo1niya <mo1niya@yandex.ru>
2019-09-06 21:15:17 +0300

    First commit

---
+++ ru.perepelitsina/part1/lesson02/task3/WithoutArgs3.java
@@ -0,0 +1,8 @@
+package ru.perepelitsina.part1.lesson02.task3;
+
+public class WithoutArgs3 {
+    public static void main(String[] args) {
+        int sec = 3600;
+        System.out.println(sec/60/60 + " час");
+    }
+}
