7819ca5a8fbd0e2e66c175bfced749be04b71586
mo1niya <mo1niya@yandex.ru>
2019-09-10 19:39:47 +0300

    2

--- ru.perepelitsina/part1/lesson03/Task3.java
+++ ru.perepelitsina/part1/lesson03/Task3.java
@@ -8,5 +8,6 @@ public class Task3 {
         System.out.print("Введите количество секунд: ");
         float sec = in.nextFloat();
         System.out.println(sec/60/60 + " час");
+        in.close();
     }
 }
