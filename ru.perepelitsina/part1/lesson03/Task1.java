50964fe86c83c1ce5aa136411dba241a8069f25b
mo1niya <mo1niya@yandex.ru>
2019-09-10 19:06:52 +0300

    1

---
+++ ru.perepelitsina/part1/lesson03/Task1.java
@@ -0,0 +1,12 @@
+package ru.perepelitsina.part1.lesson03;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите количество литров: ");
+        float liters = in.nextFloat();
+        System.out.println(liters*43 + " рубрей");
+    }
+}
