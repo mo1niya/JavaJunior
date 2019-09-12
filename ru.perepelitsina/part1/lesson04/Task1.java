aaec6c293022488e2d053d352434a66ae6a39206
mo1niya <mo1niya@yandex.ru>
2019-09-12 12:18:58 +0300

    2

--- ru.perepelitsina/part1/lesson04/Task1.java
+++ ru.perepelitsina/part1/lesson04/Task1.java
@@ -6,16 +6,20 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        int f = in.nextInt();
-        System.out.println("Введите второе+ число");
-        int s = in.nextInt();
-        if (f>s){
-            System.out.println("Меньшее число - " + s);
-        } else if (s>f){
-            System.out.println("Меньшее число - " + f);
-        } else {
-            System.out.println("Числа равны");
+        System.out.println("Введите два числа");
+        try {
+            int f = in.nextInt();
+            int s = in.nextInt();
+            if (f>s){
+                System.out.println("Меньшее число - " + s);
+            } else if (s>f){
+                System.out.println("Меньшее число - " + f);
+            } else {
+                System.out.println("Числа равны");
+            }
+        } catch (Exception e){
+            System.out.println("Это не число");
+            System.exit(0);
         }
         in.close();
     }
