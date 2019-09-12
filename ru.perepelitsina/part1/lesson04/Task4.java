253e27a92b9628303596e1ac0ace1e1fa931fada
mo1niya <mo1niya@yandex.ru>
2019-09-12 12:19:04 +0300

    2

--- ru.perepelitsina/part1/lesson04/Task4.java
+++ ru.perepelitsina/part1/lesson04/Task4.java
@@ -7,18 +7,23 @@ public class Task4 {
     public static void main(String[] args) {
         System.out.println("Введите число");
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.println("Арифметическая прогрессия:");
-        int a = 1;
-        for (int i = 1; i<=n; i++){
-            System.out.println("a" + i + " = " + a + " + " + 2 + " = " + (a+2));
-            a = a+2;
-        }
-        System.out.println("Геометрическая прогрессия:");
-        int b = 1;
-        for (int i = 1; i<=n; i++){
-            System.out.println("b" + i + " = " + b + " * " + 2 + " = " + (b*2));
-            b = b*2;
+        try {
+            int n = in.nextInt();
+            System.out.println("Арифметическая прогрессия:");
+            int a = 1;
+            for (int i = 1; i<=n; i++){
+                System.out.println("a" + i + " = " + a + " + " + 2 + " = " + (a+2));
+                a = a+2;
+            }
+            System.out.println("Геометрическая прогрессия:");
+            int b = 1;
+            for (int i = 1; i<=n; i++){
+                System.out.println("b" + i + " = " + b + " * " + 2 + " = " + (b*2));
+                b = b*2;
+            }
+        } catch (Exception e){
+            System.out.println("Это не число");
+            System.exit(0);
         }
         in.close();
     }
