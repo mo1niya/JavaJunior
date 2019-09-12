c52f46e879c2fcdec7ee812afa9ff00b775c7138
mo1niya <mo1niya@yandex.ru>
2019-09-12 11:51:22 +0300

    1

---
+++ ru.perepelitsina/part1/lesson04/Task2.java
@@ -0,0 +1,31 @@
+package ru.perepelitsina.part1.lesson04;
+
+import java.util.Scanner;
+
+//Написать программу, которая описывает введенное число.
+// Отрицательное оно число или положительное (или нулевое), чётное или нечётное.
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Введите число ");
+        Scanner in = new Scanner(System.in);
+        try {
+            float num = in.nextFloat();
+            if (num>0){
+                System.out.println("Положительное число");
+            } else if (num<0){
+                System.out.println("Отрицательное число");
+            } else if (num==0) {
+                System.out.println("Это 0");
+            }
+
+            if ((num%2) == 0){
+                System.out.println("Чётное число");
+            } else {
+                System.out.println("Нечётное число");
+            }
+        } catch (Exception e){
+            System.out.println("Это не число");
+        }
+        in.close();
+    }
+}
