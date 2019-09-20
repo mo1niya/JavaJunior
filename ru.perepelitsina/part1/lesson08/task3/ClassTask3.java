f5147f7a15f5ce1dad4a496b54b5b24f6e32f0d3
mo1niya <mo1niya@yandex.ru>
2019-09-20 17:26:46 +0300

    1

---
+++ ru.perepelitsina/part1/lesson08/task3/ClassTask3.java
@@ -0,0 +1,10 @@
+package ru.perepelitsina.part1.lesson08.task3;
+
+public class ClassTask3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            new Task3();
+        }
+        System.out.println(Task3.count);
+    }
+}
