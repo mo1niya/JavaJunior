f51aca49eeef1e215dc59c4b6064a2ff9eeadf41
mo1niya <mo1niya@yandex.ru>
2019-09-20 17:14:14 +0300

    1

---
+++ ru.perepelitsina/part1/lesson08/task4/Contract.java
@@ -0,0 +1,27 @@
+package ru.perepelitsina.part1.lesson08.task4;
+
+public class Contract {
+    private int number;
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    private String data;
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    private String[] products;
+    public String[] getProducts() {
+        return products;
+    }
+    public void setProducts(String[] products) {
+        this.products = products;
+    }
+}
