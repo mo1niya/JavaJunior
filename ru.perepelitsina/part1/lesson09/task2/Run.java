5384ba9296545c065523636b1d2f7d239b392053
mo1niya <mo1niya@yandex.ru>
2019-09-23 12:12:55 +0300

    1

---
+++ ru.perepelitsina/part1/lesson09/task2/Run.java
@@ -0,0 +1,5 @@
+package ru.perepelitsina.part1.lesson09.task2;
+
+public interface Run {
+    String Run();
+}
---
+++ ru.perepelitsina/part1/lesson09/task2/Swim.java
@@ -0,0 +1,5 @@
+package ru.perepelitsina.part1.lesson09.task2;
+
+public interface Swim {
+    String Swim();
+}
