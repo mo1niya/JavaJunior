176befa0fb6eed16ab9195e84bb01c344c0f7df5
mo1niya <mo1niya@yandex.ru>
2019-10-15 18:05:28 +0300

    2

---
+++ ru.perepelitsina/part1/lesson18/task1.java
@@ -0,0 +1,21 @@
+package ru.perepelitsina.part1.lesson18;
+
+import java.io.*;
+
+//Написать программу, которая копирует файл с одной кодировкой в файл с другой.
+public class task1 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader (new FileInputStream("UTF8.txt"), "UTF8"));
+        OutputStreamWriter ow = new OutputStreamWriter(new FileOutputStream("windows-1251.txt"), "koi8")){
+            String ln;
+            while ((ln = br.readLine()) != null){
+                System.out.print(ln + "\n");
+                ow.write(ln + "\n");
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("Файл не найден!");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
