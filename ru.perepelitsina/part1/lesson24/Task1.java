b34e20766b9b9d3827f6219f6ed8de8ea11f50ef
mo1niya <mo1niya@yandex.ru>
2019-10-26 20:19:09 +0300

    1

---
+++ ru.perepelitsina/part1/lesson24/Task1.java
@@ -0,0 +1,34 @@
+package ru.perepelitsina.part1.lesson24;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Написать метод, который возвращает множество, в котором удалены все элементы четной длины из исходного множества.
+//public Set<String> removeEvenLength(Set<String> set);
+//Например, для множества ["foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"] метод вернет ["foo", "bar", "spoon", "!"]
+public class Task1 {
+    public static void main(String[] args) {
+        Set<String> strings = new HashSet<>();
+        strings.add("foo");
+        strings.add("buzz");
+        strings.add("bar");
+        strings.add("fork");
+        strings.add("bort");
+        strings.add("spoon");
+        strings.add("!");
+        strings.add("dude");
+        for (String s: removeEvenLength(strings)){
+            System.out.println(s);
+        }
+    }
+    static public Set<String> removeEvenLength(Set<String> set){
+        Set<String> tmp = new HashSet<>();
+        for (String str: set){
+            if (str.length()%2 == 0){
+                tmp.add(str);
+            }
+        }
+        set.removeAll(tmp);
+        return set;
+    }
+}
