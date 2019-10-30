3fef5eb7e562d9261533940925caa2c3a4d4bb27
mo1niya <mo1niya@yandex.ru>
2019-10-30 13:04:03 +0300

    1

---
+++ ru.perepelitsina/part1/lesson25/Task1.java
@@ -0,0 +1,43 @@
+package ru.perepelitsina.part1.lesson25;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//public boolean isUnique(Map<String, String> map);
+//Написать метод, который возвращает true, если в мапе нет двух и более одинаковых value, и false в противном случае.
+//Для пустой мапы метод возвращает true.
+//Например, для метода {Вася=Иванов, Петр=Петров, Виктор=Сидоров, Сергей=Савельев, Вадим=Викторов} метод вернет true,
+//а для {Вася=Иванов, Петр=Петров, Виктор=Иванов, Сергей=Савельев, Вадим=Петров} метод вернет false.
+public class Task1 {
+    public static void main(String[] args) {
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("Вася", "Иванов");
+        map1.put("Петр", "Петров");
+        map1.put("Виктор", "Сидоров");
+        map1.put("Сергей", "Савельев");
+        map1.put("Вадим", "Викторов");
+        System.out.println(isUnique(map1));
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("Вася", "Иванов");
+        map2.put("Петр", "Петров");
+        map2.put("Виктор", "Иванов");
+        map2.put("Сергей", "Савельев");
+        map2.put("Вадим", "Петров");
+        System.out.println(isUnique(map2));
+        Map<String, String> map3 = new HashMap<>();
+        System.out.println(isUnique(map3));
+    }
+    private static boolean isUnique(Map<String, String> map){
+        boolean tmp = true;
+        String val = "";
+        for (Map.Entry<String, String> entry: map.entrySet()){
+            String entryVal = entry.getValue();
+            if (val == entryVal){
+                return false;
+            } else {
+                val = entryVal;
+            }
+        }
+        return tmp;
+    }
+}
